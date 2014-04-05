5961ee88dcf970da7b1d6cf5877d42e411d908cd
barneycolby <barnaby_colby@hotmail.com>
2014-04-05 11:49:29 +0100

    Added scaladoc for Header class

--- src/Header.scala
+++ src/Header.scala
@@ -2,6 +2,14 @@ package DMGExtractor
 
 import java.io.RandomAccessFile
 
+/**
+ * Parses the header of a DMG file and stores it's data
+ *
+ * @param headerBytes The header bytes contained in a byte array, should be 512 bytes long
+ * @throws InvalidHeaderException If the header is invalid or missing
+ *
+ * @author Barnaby Colby
+ */
 class Header(val headerBytes: Array[Byte]) {
 	// The first 4 bytes should contain the magic KOLY value
 	// Construct the expected and actual magic
@@ -10,6 +18,7 @@ class Header(val headerBytes: Array[Byte]) {
 
 	// Check whether they're equal
 	if (!actualMagic.sameElements(expectedMagic)) {
+		println("["+actualMagic(0)+","+actualMagic(1)+","+actualMagic(2)+","+actualMagic(3)+"]")
 		throw new InvalidHeaderException(InvalidHeaderExceptionType.Missing)
 	}
 }
